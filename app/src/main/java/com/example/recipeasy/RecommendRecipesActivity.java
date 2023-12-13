d11303120a7d8c9c5235a8b49791672ca25b595c
Sude Ergün <sudeergun03@gmail.com>
2023-12-13 16:41:20 +0300

    recipes
    recipes xml is added

--- app/src/main/java/com/example/recipeasy/RecommendRecipesActivity.java
+++ app/src/main/java/com/example/recipeasy/RecommendRecipesActivity.java
@@ -13,6 +13,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class RecommendRecipesActivity extends AppCompatActivity {
     private ImageButton backButton;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
