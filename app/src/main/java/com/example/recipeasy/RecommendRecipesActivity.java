16cfa4ac811366a643ec683e19f1fd4d25d5a537
Sude Ergün <sudeergun03@gmail.com>
2023-12-13 20:46:02 +0300

    recipeRecommendation
    buttons' events are created.

--- app/src/main/java/com/example/recipeasy/RecommendRecipesActivity.java
+++ app/src/main/java/com/example/recipeasy/RecommendRecipesActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -13,7 +14,24 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class RecommendRecipesActivity extends AppCompatActivity {
     private ImageButton backButton;
-
+    private Button servingsButton;
+    private Button timeButton;
+    private Button filterButton;
+    private Button sortButton;
+    private CheckBox servings_checkbox1;
+    private CheckBox servings_checkbox2;
+    private CheckBox servings_checkbox3;
+    private CheckBox servings_checkbox4;
+    private CheckBox servings_checkbox5;
+    private CheckBox time_checkbox1;
+    private CheckBox time_checkbox2;
+    private CheckBox time_checkbox3;
+    private CheckBox time_checkbox4;
+    private CheckBox time_checkbox5;
+    private CheckBox sort_checkbox1;
+    private CheckBox sort_checkbox2;
+    private CheckBox sort_checkbox3;
+    private CheckBox sort_checkbox4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,5 +84,92 @@ public class RecommendRecipesActivity extends AppCompatActivity {
         });
 
 
+        filterButton = findViewById(R.id.btnFilter);
+        sortButton = findViewById(R.id.btnSort);
+        servingsButton = findViewById(R.id.btnServings);
+        timeButton = findViewById(R.id.btnTime);
+        sort_checkbox1 = findViewById(R.id.checkbox_sort_time1);
+        sort_checkbox2 = findViewById(R.id.checkbox_sort_time2);
+        sort_checkbox3 = findViewById(R.id.checkbox_sort_servings1);
+        sort_checkbox4 = findViewById(R.id.checkbox_sort_servings2);
+        servings_checkbox1 = findViewById(R.id.checkbox_2serv);
+        servings_checkbox2 = findViewById(R.id.checkbox_4serv);
+        servings_checkbox3 = findViewById(R.id.checkbox_6serv);
+        servings_checkbox4 = findViewById(R.id.checkbox_8serv);
+        servings_checkbox5 = findViewById(R.id.checkbox_8plus_serv);
+        time_checkbox1 = findViewById(R.id.checkbox_filter_time1);
+        time_checkbox2 = findViewById(R.id.checkbox_filter_time2);
+        time_checkbox3 = findViewById(R.id.checkbox_filter_time3);
+        time_checkbox4 = findViewById(R.id.checkbox_filter_time4);
+        time_checkbox5 = findViewById(R.id.checkbox_filter_time5);
+
+        sortButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle single serving button click
+                showSortCheckboxes();
+            }
+        });
+
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle single serving button click
+                showFilterOptions();
+            }
+        });
+
+        servingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle single serving button click
+                showFilterServingsCheckboxes();
+            }
+        });
+
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle single serving button click
+                showFilterTimeCheckboxes();
+            }
+        });
+
+
+    }
+    private void showSortCheckboxes() {
+        // Show checkboxes when servings button is clicked
+        sort_checkbox1.setVisibility(View.VISIBLE);
+        sort_checkbox2.setVisibility(View.VISIBLE);
+        sort_checkbox3.setVisibility(View.VISIBLE);
+        sort_checkbox4.setVisibility(View.VISIBLE);
+        // Make more checkboxes visible as needed
+    }
+
+    private void showFilterOptions() {
+        // Show checkboxes when servings button is clicked
+        servingsButton.setVisibility(View.VISIBLE);
+        timeButton.setVisibility(View.VISIBLE);
+        // Make more checkboxes visible as needed
+    }
+
+    private void showFilterServingsCheckboxes() {
+        // Show checkboxes when servings button is clicked
+        servings_checkbox1.setVisibility(View.VISIBLE);
+        servings_checkbox2.setVisibility(View.VISIBLE);
+        servings_checkbox3.setVisibility(View.VISIBLE);
+        servings_checkbox4.setVisibility(View.VISIBLE);
+        servings_checkbox5.setVisibility(View.VISIBLE);
+        // Make more checkboxes visible as needed
+    }
+
+    private void showFilterTimeCheckboxes() {
+        // Show checkboxes when servings button is clicked
+        time_checkbox1.setVisibility(View.VISIBLE);
+        time_checkbox2.setVisibility(View.VISIBLE);
+        time_checkbox3.setVisibility(View.VISIBLE);
+        time_checkbox4.setVisibility(View.VISIBLE);
+        time_checkbox5.setVisibility(View.VISIBLE);
+        // Make more checkboxes visible as needed
     }
 }
