b204750aec936874bd094afe02c6ae40289cf23e
Sude Ergün <sudeergun03@gmail.com>
2023-12-15 10:48:11 +0300

    favorites
    favorites navigation bar is added

--- app/src/main/java/com/example/recipeasy/FavoritesActivity.java
+++ app/src/main/java/com/example/recipeasy/FavoritesActivity.java
@@ -2,7 +2,10 @@ package com.example.recipeasy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class FavoritesActivity extends AppCompatActivity {
 
@@ -10,5 +13,36 @@ public class FavoritesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        bottomNavigationView.setSelectedItemId(R.id.favorites);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.favorites) {
+                return true;
+            } else if (itemId == R.id.shopping_cart) {
+                startActivity(new Intent(getApplicationContext(), ShoppingListActivity.class));
+                //overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (itemId == R.id.fridge) {
+                startActivity(new Intent(getApplicationContext(), FridgeActivity.class));
+                //overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (itemId == R.id.recipe) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                //overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (itemId == R.id.settings) {
+                startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                //overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            }
+            return false;
+        });
     }
 }
