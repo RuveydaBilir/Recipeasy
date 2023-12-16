67fab628fb8d654909edf59797d642164d80a925
Rüveyda Bilir <rvydblr@gmail.com>
2023-12-16 17:41:16 +0300

    Fridge search
    is implemented (with some mistakes)

--- app/src/main/java/com/example/recipeasy/FridgeActivity.java
+++ app/src/main/java/com/example/recipeasy/FridgeActivity.java
@@ -8,6 +8,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.recipeasy.BackEnd.Fridge;
 import com.example.recipeasy.BackEnd.Recipe;
@@ -22,6 +24,8 @@ public class FridgeActivity extends AppCompatActivity {
     //DatabaseReference database;
     FridgeAdapter fridgeAdapter;
     ArrayList<String> category;
+
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +72,16 @@ public class FridgeActivity extends AppCompatActivity {
                 return true;
             }
             return false;
+        });
+
+        addButton = findViewById(R.id.fridge_add_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Fridge_SearchForIngredientActivity.class));
+                //overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+            }
         });
     }
 }
